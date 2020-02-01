d47a99804fade1c4af0f92dd1aafc1b3dfd8489a
Fershark <ferchriquelme@gmail.com>
2020-02-01 15:31:28 -0800

    Frontend - login code formatting

--- frontend/src/actions/authenticate.js
+++ frontend/src/actions/authenticate.js
@@ -1,115 +1,124 @@
-import { AUTH_SIGNUP_USER, 
-    API_CREATE_USER, AUTH_LOGIN_USER, AUTH_PROCESSING,
-    API_GET_USER
+import {
+  AUTH_SIGNUP_USER,
+  API_CREATE_USER,
+  AUTH_LOGIN_USER,
+  AUTH_PROCESSING,
+  API_GET_USER,
 } from '../config/endpoints-conf';
 //import axios from 'axios';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 // import * as firebase from 'firebase';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 // firebase config
 const fireBaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 const fireBaseApp = firebase.initializeApp(fireBaseConfig);
 
 // firebase sign in account
 export const doSignInWithEmailAndPassword = (email, password) => {
-    return dispatch => {
+  return dispatch => {
+    dispatch({
+      type: AUTH_PROCESSING,
+      payload: true,
+    });
+
+    fireBaseApp
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('AUTHENTICATED');
+        const message = 'Successfully signin account';
         dispatch({
-            type: AUTH_PROCESSING ,
-            payload: true
+          type: AUTH_LOGIN_USER,
+          payload: {message, success: true},
         });
 
-        fireBaseApp.auth().signInWithEmailAndPassword(email, password)
-            .then(res => {
-                console.log("AUTHENTICATED");
-                const message = 'Successfully signin account';
-                dispatch({
-                    type: AUTH_LOGIN_USER,
-                    payload: { message, success: true}
-                });
+        const user = {
+          uid: res.user.uid,
+          email: res.user.email,
+          displayName: res.user.displayName,
+          photoURL: res.user.photoURL,
+        };
 
-                const user = {
-                    uid: res.user.uid,
-                    email: res.user.email,
-                    displayName: res.user.displayName,
-                    photoURL: res.user.photoURL,
-                }
+        localStorage.setItem('app_user', JSON.stringify(user));
 
-                localStorage.setItem("app_user", JSON.stringify(user));
+        dispatch({
+          type: AUTH_PROCESSING,
+          payload: false,
+        });
+      })
+      .catch(err => {
+        console.log('ERROR AUTHENTICATED');
+        const {message} = err;
+        dispatch({
+          type: AUTH_LOGIN_USER,
+          payload: {message, success: false},
+        });
 
-                dispatch({
-                    type: AUTH_PROCESSING ,
-                    payload: false
-                });
+        dispatch({
+          type: AUTH_PROCESSING,
+          payload: false,
+        });
 
-            }).catch(err => {
-                console.log("ERROR AUTHENTICATED");
-                const { message } = err;
-                dispatch({
-                    type: AUTH_LOGIN_USER,
-                    payload: { message, success: false}
-                });
-
-                dispatch({
-                    type: AUTH_PROCESSING ,
-                    payload: false
-                });
-
-                toast.error(message);
-            });
-    }
-}
+        toast.error(message);
+      });
+  };
+};
 
 // firebase signout
-export const doSignOut = (props) => {
-    fireBaseApp.auth().signOut();
-    localStorage.setItem("app_user", null);
-    props.history.push('/login');
-}
+export const doSignOut = props => {
+  fireBaseApp.auth().signOut();
+  localStorage.setItem('app_user', null);
+  props.history.push('/login');
+};
 
 // get current Auth User
 export const getCurrentUserAuth = () => {
-    return new Promise ((resolve, reject) => {
-        fireBaseApp.auth().onAuthStateChanged(user => {
-            if (user) {
-                console.log('user', user);
+  return new Promise((resolve, reject) => {
+    fireBaseApp.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('user', user);
 
-                resolve({
-                    displayName: user.displayName,
-                    email: user.email
-                })
-            } else {
-                resolve(null);
-            }
+        resolve({
+          displayName: user.displayName,
+          email: user.email,
         });
-    })
-}
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
 
 export const getUserIdToken = () => {
-    return new Promise ((resolve, reject) => {
-        fireBaseApp.auth().onAuthStateChanged(user => {
-            if (user) {
-                fireBaseApp.auth().currentUser.getIdToken(true)
-                    .then(idToken => {
-                        resolve({idToken})
-                    }).catch(err => {
-                        reject(err)
-                    })
-            } else {
-                resolve(null)
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    fireBaseApp.auth().onAuthStateChanged(user => {
+      if (user) {
+        fireBaseApp
+          .auth()
+          .currentUser.getIdToken(true)
+          .then(idToken => {
+            resolve({idToken});
+          })
+          .catch(err => {
+            reject(err);
+          });
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
 
 // firebase sign up account
 /*
--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -1,132 +1,150 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import { Avatar, Link, Grid, Container, LinearProgress, CssBaseline, Typography, TextField, Button } from '@material-ui/core';
+import {
+  Avatar,
+  Link,
+  Grid,
+  Container,
+  LinearProgress,
+  CssBaseline,
+  Typography,
+  TextField,
+  Button,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import styles from '../assets/jss/views/login';
-import { AUTH_LOGIN_USER } from '../config/endpoints-conf';
-import { ToastContainer } from 'react-toastify';
-import { doSignInWithEmailAndPassword, getCurrentUserAuth } from '../actions/authenticate';
+import {AUTH_LOGIN_USER} from '../config/endpoints-conf';
+import {ToastContainer} from 'react-toastify';
+import {
+  doSignInWithEmailAndPassword,
+  getCurrentUserAuth,
+} from '../actions/authenticate';
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleLogin = this.handleLogin.bind(this);
-    }
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    componentDidMount() {
-        this.props.reset();
-        getCurrentUserAuth()
-            .then(user => {
-                if (user) {
-                    console.log("USER SIGNED IN, SO REDIRECT TO HOME PAGE");
-                    this.props.history.push('/');
-                } else {
-                    console.log("USER NOT LOGIN YET");
-                    this.props.history.push('/login');
-                }
-            })
-    }
+  componentDidMount() {
+    this.props.reset();
+    getCurrentUserAuth().then(user => {
+      if (user) {
+        console.log('USER SIGNED IN, SO REDIRECT TO HOME PAGE');
+        this.props.history.push('/');
+      } else {
+        console.log('USER NOT LOGIN YET');
+        this.props.history.push('/login');
+      }
+    });
+  }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.auth_message.success !== this.props.auth_message.success) {
-            this.props.history.push('/');
-        }
+  componentDidUpdate(prevProps) {
+    if (prevProps.auth_message.success !== this.props.auth_message.success) {
+      this.props.history.push('/');
     }
+  }
 
-    handleLogin (e) {
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        //doSignInWithEmailAndPassword(email, password);
-        this.props.login(email, password);
-        e.preventDefault();
-    }
+  handleLogin(e) {
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    this.props.login(email, password);
+    e.preventDefault();
+  }
 
-    render () {
-        const { classes } = this.props;
-        return (
-            <React.Fragment>
-            { this.props.auth_processing && <LinearProgress color="secondary" /> }
-            <Container component="main" maxWidth="xs">
-                <ToastContainer />
-                <CssBaseline />
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <form className={classes.form} noValidate onSubmit={this.handleLogin}>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container className={classes.gridContainer}>
-                            <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                            </Grid>
-                            <Grid item>
-                            <Link href="/signup" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
-            </Container>
-            </React.Fragment>
-        )
-    }
+  render() {
+    const {classes} = this.props;
+    return (
+      <React.Fragment>
+        {this.props.auth_processing && <LinearProgress color="secondary" />}
+        <Container component="main" maxWidth="xs">
+          <ToastContainer />
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form
+              className={classes.form}
+              noValidate
+              onSubmit={this.handleLogin}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}>
+                Sign In
+              </Button>
+              <Grid container className={classes.gridContainer}>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 Login.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
+  classes: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = state => {
-    return {
-        auth_processing: state.auth.auth_processing,
-        auth_message: state.auth.auth_message
-    }
-}
+  return {
+    auth_processing: state.auth.auth_processing,
+    auth_message: state.auth.auth_message,
+  };
+};
 
 const mapDispatchtoProps = dispatch => {
-    return {
-        login: (email, password) => dispatch(doSignInWithEmailAndPassword(email, password)),
-        reset: () => dispatch({type: AUTH_LOGIN_USER, payload: { message: '', success: false}})
-    }
-}
+  return {
+    login: (email, password) =>
+      dispatch(doSignInWithEmailAndPassword(email, password)),
+    reset: () =>
+      dispatch({type: AUTH_LOGIN_USER, payload: {message: '', success: false}}),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchtoProps)((withStyles(styles)(Login)));
+export default connect(
+  mapStateToProps,
+  mapDispatchtoProps,
+)(withStyles(styles)(Login));
