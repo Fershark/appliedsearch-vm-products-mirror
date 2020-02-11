a0b92498aee53232746ef05ecf4d4c8d36baf55a
Fershark <ferchriquelme@gmail.com>
2020-02-11 11:48:21 -0800

    Frontend - Material Table added

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Drawer} from './Drawer';
+export {default as AppBar} from './AppBar';
+export {default as Title} from './Title';
+export {default as MaterialTable} from './MaterialTable';
