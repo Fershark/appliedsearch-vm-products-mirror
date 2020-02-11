68ccd0e6e9aadc635b0bd5f21c32f3be9ee5d6da
VuDuyHauNguyen <nguyenv19@student.douglascollege.ca>
2020-02-10 17:45:15 -0800

    updated database schema and mock-up

--- backend/database/mockup.sql
+++ backend/database/mockup.sql
@@ -1,1 +1,17 @@
 USE appliedreseach_vm;
+
+insert into USERS (id, uid, name, email, phone, address) values (1, 'iEc71sIHIsdqE9eJ5oqvzz1j6cT2', 'Hau Nguyen', 'nvdhau@gmail.com', '777888999', '123 Royal Ave');
+insert into USERS (id, uid, name, email, phone, address) values (2, '5zNt8xfZS0fgDBJv2VpJrkruXNg2', 'Quang Le', 'quang.le205@gmail.com', '111222333', '789 Douglas Road');
+
+insert into PRODUCTS (name, description, version) values ('MySQL', 'Relational Datatabase Management System', '5.5');
+insert into PRODUCTS (name, description, version) values ('MySQL', 'Relational Datatabase Management System', '5.6');
+insert into PRODUCTS (name, description, version) values ('MySQL', 'Relational Datatabase Management System', '5.7');
+insert into PRODUCTS (name, description, version) values ('MySQL', 'Relational Datatabase Management System', '8.0');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '5.4');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '5.5');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '5.6');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.0');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.1');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.2');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.3');
+insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.4');
