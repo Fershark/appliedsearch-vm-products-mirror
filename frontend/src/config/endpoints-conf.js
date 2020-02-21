4470c92027f2d467b172fd67748147c56199ecf7
Fershark <ferchriquelme@gmail.com>
2020-02-21 02:24:38 -0800

    Frontend - proxy added for communication with back URLS cleaning

--- frontend/src/config/endpoints-conf.js
+++ frontend/src/config/endpoints-conf.js
@@ -2,12 +2,8 @@
 export const AUTH_LOGIN_USER = 'AUTH_LOGIN_USER';
 export const AUTH_PROCESSING  = 'AUTH_PROCESSING';
 export const AUTH_LOGOUT = 'AUTH_LOGOUT';
-export const SET_CATEGORIES  = 'SET_CATEGORIES';
 
 // create new user
-export const API_ROOT_URL = process.env.REACT_APP_ROOT_ENDPOINT_URL ? process.env.REACT_APP_ROOT_ENDPOINT_URL : "http://127.0.0.1:3000";
+export const API_ROOT_URL = process.env.REACT_APP_ROOT_ENDPOINT_URL ? process.env.REACT_APP_ROOT_ENDPOINT_URL : "";
 export const API_CREATE_USER = API_ROOT_URL + "/api/users/create";
 export const API_GET_USER = API_ROOT_URL + "/api/users/";
-export const API_GET_CATEGORIES = API_ROOT_URL + "/api/categories/";
-export const API_CREATE_POST = API_ROOT_URL + "/api/posts/create";
-export const API_CREATE_VIDEOPOST =  API_ROOT_URL + "/api/posts/create-video";
