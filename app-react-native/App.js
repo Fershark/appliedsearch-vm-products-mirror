992c3de6abc3bf9fdc16bc736192ff1d10e86e5d
Fershark <ferchriquelme@gmail.com>
2020-03-14 15:26:35 -0700

    App - initial project

---
+++ app-react-native/App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Login from './screens/Login';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{title: 'Login'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
