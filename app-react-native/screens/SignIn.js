6e48480979420dd23d67cc41a126c75c42f98070
Fershark <ferchriquelme@gmail.com>
2020-03-15 16:59:06 -0700

    App - Sign in screen

---
+++ app-react-native/screens/SignIn.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {TextInput, Button, HelperText} from 'react-native-paper';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const submit = () => {
+    let valid = true;
+
+    if (email === '') {
+      setEmailError('Email address is required');
+      valid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (password === '') {
+      setPasswordError('Password is required');
+      valid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    if (valid) {
+      setLoading(true);
+      
+    }
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.marginBottom}>
+        <TextInput
+          label="Email address"
+          onChangeText={text => setEmail(text)}
+          value={email}
+          autoFocus={true}
+          keyboardType="email-address"
+        />
+        <HelperText type="error" visible={emailError !== ''}>
+          {emailError}
+        </HelperText>
+      </View>
+      <View style={styles.marginBottom}>
+        <TextInput label="Password" onChangeText={text => setPassword(text)} value={password} secureTextEntry={true} />
+        <HelperText type="error" visible={passwordError !== ''}>
+          {passwordError}
+        </HelperText>
+      </View>
+      <Button
+        style={styles.marginBottom}
+        mode="contained"
+        loading={loading}
+        disabled={loading}
+        onPress={() => submit()}>
+        Submit
+      </Button>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  marginBottom: {
+    marginBottom: 20,
+  },
+});
