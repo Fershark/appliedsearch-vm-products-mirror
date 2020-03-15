77712cad578f3ae9612c2191b733d59cfb074d9c
Fershark <ferchriquelme@gmail.com>
2020-03-15 16:58:05 -0700

    App - react paper added to the project

--- app-react-native/App.js
+++ app-react-native/App.js
@@ -1,22 +1,24 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { AppRegistry } from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import Login from './screens/Login';
+import SignIn from './screens/SignIn';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{title: 'Login'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="SignIn" component={SignIn} options={{title: 'Sign In'}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
+
+AppRegistry.registerComponent('main', () => Main);
