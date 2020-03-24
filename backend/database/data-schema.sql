ad687d0ab6952b3dd4bb8700fe58f599f378acb3
nvdhau <nvdhau@gmail.com>
2020-03-23 23:07:40 -0700

    added more mockup products data

--- backend/database/data-schema.sql
+++ backend/database/data-schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE PRODUCTS (
      id INTEGER AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR(255),
      description TEXT,
-     `version` VARCHAR(255)
+     'version' VARCHAR(255)
 );
 
 CREATE TABLE VMS(
@@ -52,3 +52,49 @@ insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded 
 insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.2');
 insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.3');
 insert into PRODUCTS (name, description, version) values ('PHP', 'HTML-embedded scripting language', '7.4');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '4.2');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '4.0');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '3.6');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '3.4');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '3.2');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '3.0');
+insert into PRODUCTS (name, description, version) values ('MongoDB', 'A document database with the scalability and flexibility that you want with the querying and indexing that you need', '2.6');
+insert into PRODUCTS (name, description, version) values ('Apache', 'A free and open-source cross-platform web server software, released under the terms of Apache License 2.0. Apache is developed and maintained by an open community of developers under the auspices of the Apache Software Foundation.', '2.4');
+insert into PRODUCTS (name, description, version) values ('Apache', 'A free and open-source cross-platform web server software, released under the terms of Apache License 2.0. Apache is developed and maintained by an open community of developers under the auspices of the Apache Software Foundation.', '2.2');
+insert into PRODUCTS (name, description, version) values ('Apache', 'A free and open-source cross-platform web server software, released under the terms of Apache License 2.0. Apache is developed and maintained by an open community of developers under the auspices of the Apache Software Foundation.', '2.0');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.16.1');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.14.2');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.12.2');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.10.3');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.8.1');
+insert into PRODUCTS (name, description, version) values ('Nginx', 'A web server which can also be used as a reverse proxy, load balancer, mail proxy and HTTP cache.', '1.6.3');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '13.11.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '13.10.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '13.9.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '13.8.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '12.16.1');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '12.16.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '12.15.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '12.14.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '12.13.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '11.15.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '10.19.0');
+insert into PRODUCTS (name, description, version) values ('Node.js', 'A JavaScript runtime built on Chrome''s V8 JavaScript engine.', '9.11.2');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.8.2');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.7.7');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.6.10');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.5.9');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.4.10');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '3.3.7');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '2.7.16');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '2.7.6');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '2.7.2');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '2.6.7');
+insert into PRODUCTS (name, description, version) values ('Python', 'An interpreted, high-level, general-purpose programming language.', '2.5.6');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '5.3');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '5.2');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '5.1');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '4.9');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '4.5');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '4.0');
+insert into PRODUCTS (name, description, version) values ('WordPress', 'A free and open-source content management system (CMS) written in PHP[4] and paired with a MySQL or MariaDB database.', '3.9');
