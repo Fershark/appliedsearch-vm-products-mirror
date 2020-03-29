0178af0d54cfa0fb047fdac3f062eaaa577b9c0d
Fershark <ferchriquelme@gmail.com>
2020-03-29 00:59:17 -0700

    App - Adding VM - Distribution and Sizes

--- app-react-native/navigation/DrawerNavigator.js
+++ app-react-native/navigation/DrawerNavigator.js
@@ -6,6 +6,8 @@ import {Button} from 'react-native-paper';
 import {Appbar, DrawerContent} from '../components';
 import Home from '../screens/Home';
 import SignOut from '../screens/SignOut';
+import VMs from '../screens/VMs';
+import VM from '../screens/VM';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -21,11 +23,24 @@ function HomeNavigation({navigation}) {
   );
 }
 
+function VMNavigation({navigation}) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        header: headerProps => <Appbar {...headerProps} />,
+      }}>
+      <Stack.Screen name="VMs" component={VMs} options={{title: 'Virtual Machines'}} />
+      <Stack.Screen name="VM" component={VM} options={{title: 'Add Virtual Machine'}} />
+    </Stack.Navigator>
+  );
+}
+
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />} initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeNavigation} options={{drawerIcon: 'home'}}/>
-      <Drawer.Screen name="Sign out" component={SignOut} options={{drawerIcon: 'logout'}}/>
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+      <Drawer.Screen name="Virtual Machines" component={VMNavigation} options={{drawerIcon: 'laptop'}} />
+      <Drawer.Screen name="Home" component={HomeNavigation} options={{drawerIcon: 'home'}} />
+      <Drawer.Screen name="Sign out" component={SignOut} options={{drawerIcon: 'logout'}} />
     </Drawer.Navigator>
   );
 }
